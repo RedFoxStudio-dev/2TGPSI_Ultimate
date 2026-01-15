74d7cd1be94a9b0a049813972dab684f88400747
Red Eyes Surveytime <redsurveytime@gmail.com>
2026-01-15 21:01:42 +0000

    primeiro commit

---
+++ PSI/MO11/Mod11_Ex2/Ex40-Freguesias/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Ex40_Freguesias
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboDistrito = new System.Windows.Forms.ComboBox();
+            this.cboConcelho = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstFreguesias = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Distrito";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Concelho";
+            // 
+            // cboDistrito
+            // 
+            this.cboDistrito.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDistrito.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cboDistrito.FormattingEnabled = true;
+            this.cboDistrito.Location = new System.Drawing.Point(12, 40);
+            this.cboDistrito.Name = "cboDistrito";
+            this.cboDistrito.Size = new System.Drawing.Size(157, 21);
+            this.cboDistrito.TabIndex = 2;
+            this.cboDistrito.SelectedIndexChanged += new System.EventHandler(this.cboDistrito_SelectedIndexChanged);
+            // 
+            // cboConcelho
+            // 
+            this.cboConcelho.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboConcelho.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cboConcelho.FormattingEnabled = true;
+            this.cboConcelho.Location = new System.Drawing.Point(195, 40);
+            this.cboConcelho.Name = "cboConcelho";
+            this.cboConcelho.Size = new System.Drawing.Size(157, 21);
+            this.cboConcelho.TabIndex = 3;
+            this.cboConcelho.SelectedIndexChanged += new System.EventHandler(this.cboConcelho_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Freguesias";
+            // 
+            // lstFreguesias
+            // 
+            this.lstFreguesias.FormattingEnabled = true;
+            this.lstFreguesias.HorizontalScrollbar = true;
+            this.lstFreguesias.Location = new System.Drawing.Point(12, 95);
+            this.lstFreguesias.Name = "lstFreguesias";
+            this.lstFreguesias.ScrollAlwaysVisible = true;
+            this.lstFreguesias.Size = new System.Drawing.Size(340, 147);
+            this.lstFreguesias.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 261);
+            this.Controls.Add(this.lstFreguesias);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboConcelho);
+            this.Controls.Add(this.cboDistrito);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Freguesias";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboDistrito;
+        private System.Windows.Forms.ComboBox cboConcelho;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstFreguesias;
+    }
+}
+
---
+++ PSI/MO11/Mod11_Ex2/Ex40-Freguesias/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Ex40_Freguesias
+{
+    public partial class Form1 : Form
+    {
+        string caminhoDistrito = "Distritos.txt";
+        string caminhoConcelho = "Concelhos.txt";
+        string caminhoFreguesias = "Freguesias.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Definição das instâncias para manuseamento dos ficheiros de texto
+            FileStream FSficheiro = new FileStream(caminhoDistrito, FileMode.Open, FileAccess.Read);
+            StreamReader SR = new StreamReader(FSficheiro, System.Text.Encoding.UTF7);
+
+            //Definição da variavel que guardará uma linha do ficheiro
+            string lerlinha;  
+           
+            /*ciclo de repetição que irá ler linha a linha 
+             * até encontrar o fim do ficheiro*/
+            while (SR.Peek()!= -1)
+            {
+                //leitura de uma linha
+                lerlinha = SR.ReadLine();
+                //Escrevero seu conteúdo no controlo
+                cboDistrito.Items.Add(lerlinha);
+            }
+            //Fechar o ficheiro e libertar os recursos
+            SR.Close();
+
+        }
+
+        private void cboDistrito_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Definição das instâncias para manuseamento dos ficheiros de texto
+            FileStream FSficheiro = new FileStream(caminhoConcelho, FileMode.Open, FileAccess.Read);
+            StreamReader SR = new StreamReader(FSficheiro, System.Text.Encoding.UTF7);
+
+            //Definição da variavel que guardará uma linha do ficheiro
+            string lerlinha;  
+            //Variáveis que iram guardar o conteúdo dos conteúdos
+            string Distrito, Concelho;
+            //Variável que irá guardar a posição do delimitador
+            int p1;
+
+            //limpar o conteúdos os controlos Concelho e freguesias
+            cboConcelho.Items.Clear();
+            lstFreguesias.Items.Clear();
+
+            /*ciclo de repetição que irá ler linha a linha 
+            * até encontrar o fim do ficheiro*/
+            while (SR.Peek() != -1)
+            {
+                //Ler uma linha
+                lerlinha = SR.ReadLine();
+                
+                //Guardar a posição do delimitador            
+                p1 = lerlinha.IndexOf(";")+1;
+                //Separar o conteúdo da linha pelos campos
+                Distrito = lerlinha.Substring(0, p1 - 1);
+                Concelho = lerlinha.Substring(p1);
+
+                /*Verificar se o distrito lido coincide com o selecionado
+                 * e, em caso afirmativo, adiciona ao controlo Concelho*/
+                if(Distrito==cboDistrito.Text)
+                cboConcelho.Items.Add(Concelho);
+            }
+
+            //Fecha o ficheiro e liberta os recursos
+            SR.Close();
+
+        }
+
+        private void cboConcelho_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Definição das instâncias para manuseamento dos ficheiros de texto
+            FileStream FSficheiro = new FileStream(caminhoFreguesias, FileMode.Open, FileAccess.Read);
+            StreamReader SR = new StreamReader(FSficheiro, System.Text.Encoding.UTF7);
+            
+            //Definição da variavel que guardará uma linha do ficheiro
+            string lerlinha;
+            //Variáveis que iram guardar o conteúdo dos conteúdos
+            string Concelho, Freguesia;
+            //Variável que irá guardar a posição do delimitador
+            int p1;
+            
+            //limpar o conteúdo o controlo freguesias
+            lstFreguesias.Items.Clear();
+            /*ciclo de repetição que irá ler linha a linha 
+             * até encontrar o fim do ficheiro*/
+            while (SR.Peek() != -1)
+            {
+                //Ler uma linha
+                lerlinha = SR.ReadLine();
+                
+                //Guardar a posição do delimitador 
+                p1 = lerlinha.IndexOf(";") + 1;
+                //Separar o conteúdo da linha pelos campos
+                Concelho = lerlinha.Substring(0, p1 - 1);
+                Freguesia = lerlinha.Substring(p1);
+                /*Verificar se o concelho lido coincide com o selecionado
+                 * e, em caso afirmativo, adiciona ao controlo freguesias*/
+                if (Concelho == cboConcelho.Text)
+                    lstFreguesias.Items.Add(Freguesia);
+            }
+            //Fecha o ficheiro e liberta os recursos
+            SR.Close();
+        }
+    }
+}
