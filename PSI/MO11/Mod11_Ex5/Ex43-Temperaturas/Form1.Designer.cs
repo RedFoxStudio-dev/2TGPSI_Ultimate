a7b6b29c5fddaa657b68e1f02e60cb11e00d4572
Red Eyes Surveytime <redsurveytime@gmail.com>
2026-01-20 10:42:14 +0000

    commit MO11 Ex5

---
+++ PSI/MO11/Mod11_Ex5/Ex43-Temperaturas/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Ex43_Temperaturas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboEstacao = new System.Windows.Forms.ComboBox();
+            this.dgvTemperaturas = new System.Windows.Forms.DataGridView();
+            this.Ano = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Temp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chtGrafico = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTemperaturas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chtGrafico)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Estações Meteorológicas";
+            // 
+            // cboEstacao
+            // 
+            this.cboEstacao.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboEstacao.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cboEstacao.FormattingEnabled = true;
+            this.cboEstacao.Location = new System.Drawing.Point(30, 41);
+            this.cboEstacao.Name = "cboEstacao";
+            this.cboEstacao.Size = new System.Drawing.Size(180, 21);
+            this.cboEstacao.TabIndex = 1;
+            this.cboEstacao.SelectedIndexChanged += new System.EventHandler(this.cboCidade_SelectedIndexChanged);
+            // 
+            // dgvTemperaturas
+            // 
+            this.dgvTemperaturas.AllowUserToAddRows = false;
+            this.dgvTemperaturas.AllowUserToDeleteRows = false;
+            this.dgvTemperaturas.AllowUserToResizeColumns = false;
+            this.dgvTemperaturas.AllowUserToResizeRows = false;
+            this.dgvTemperaturas.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvTemperaturas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTemperaturas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Ano,
+            this.Temp});
+            this.dgvTemperaturas.Location = new System.Drawing.Point(30, 87);
+            this.dgvTemperaturas.Name = "dgvTemperaturas";
+            this.dgvTemperaturas.ReadOnly = true;
+            this.dgvTemperaturas.RowHeadersVisible = false;
+            this.dgvTemperaturas.Size = new System.Drawing.Size(180, 240);
+            this.dgvTemperaturas.TabIndex = 2;
+            // 
+            // Ano
+            // 
+            this.Ano.HeaderText = "Ano";
+            this.Ano.Name = "Ano";
+            this.Ano.ReadOnly = true;
+            this.Ano.Width = 75;
+            // 
+            // Temp
+            // 
+            this.Temp.HeaderText = "Temp";
+            this.Temp.Name = "Temp";
+            this.Temp.ReadOnly = true;
+            this.Temp.Width = 75;
+            // 
+            // chtGrafico
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chtGrafico.ChartAreas.Add(chartArea1);
+            this.chtGrafico.Location = new System.Drawing.Point(216, 87);
+            this.chtGrafico.Name = "chtGrafico";
+            this.chtGrafico.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.SplineArea;
+            series1.Name = "Series1";
+            this.chtGrafico.Series.Add(series1);
+            this.chtGrafico.Size = new System.Drawing.Size(438, 240);
+            this.chtGrafico.TabIndex = 3;
+            this.chtGrafico.Text = "chart1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Dados registados";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(213, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Gráfico";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 340);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chtGrafico);
+            this.Controls.Add(this.dgvTemperaturas);
+            this.Controls.Add(this.cboEstacao);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Temperatura Média Anual";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTemperaturas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chtGrafico)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboEstacao;
+        private System.Windows.Forms.DataGridView dgvTemperaturas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ano;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Temp;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chtGrafico;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PSI/MO11/Mod11_Ex5/Ex43-Temperaturas/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Ex43_Temperaturas
+{
+    public partial class Form1 : Form
+    {
+       
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //criar instância da classe e abrir o ficheiro
+            XmlDocument FicheiroXML = new XmlDocument();
+            FicheiroXML.Load("Temperaturas.xml");
+            
+            /*percorrer todos os nós elementos e escrever o nome da cidade
+            no controlo combobox*/
+            foreach (XmlNode no in FicheiroXML.SelectNodes ("//cidade"))
+                 cboEstacao.Items.Add (no.Attributes[0].Value.ToString());
+          }
+
+        private void cboCidade_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Criar uma instância da classe e abir o ficheiro
+            XmlDocument FicheiroXML = new XmlDocument();
+            FicheiroXML.Load("Temperaturas.xml");
+
+            //Definição dos arrays para os valores do grafico
+            int[] eixoX = new int[34];
+            double[] eixoY = new double[34];
+            
+            //Percorrer todos os nó elementos
+            foreach (XmlNode no in FicheiroXML.SelectNodes("//cidade"))
+            {
+               //Verificar se o nome da cidade corresponde ao atributo 
+                if (no.Attributes[0].Value.ToString ()==cboEstacao.Text)
+                {
+                    //limpar o conteúdo do controlo DataGridView
+                    dgvTemperaturas.Rows.Clear();
+                    
+                    /*Preencher os arrays com os valores e escrevê-los no controlo
+                    DataGridView*/
+                    
+                    for (int anos=0; anos<= no.ChildNodes.Count-1;anos ++)
+                    {
+                        int ano = int.Parse(no.ChildNodes[anos].Attributes[0].Value.ToString());
+                        double temp = double.Parse (no.ChildNodes[anos].Attributes[1].Value.ToString());
+                        dgvTemperaturas.Rows.Add(ano, temp);
+                        eixoX[anos]=ano;
+                        eixoY[anos]=temp;
+                    }
+                    
+                }
+            }
+
+
+            //Definição da série do gráfico
+            Series  SerieGrafico= new Series();
+            
+            SerieGrafico = chtGrafico.Series[0];
+            SerieGrafico.ChartType = SeriesChartType.SplineArea;
+            SerieGrafico.XValueType = ChartValueType.Int32;
+            SerieGrafico.YValueType = ChartValueType.Int32;
+  
+            //formatação do gráfico
+            chtGrafico.ChartAreas[0].AxisX.Minimum = 1980;
+            chtGrafico.ChartAreas[0].AxisX.Maximum = 2013;
+            chtGrafico.ChartAreas[0].AxisX.Interval = 2;
+            chtGrafico.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chtGrafico.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+            
+            //Limpar o conteúdo do gráfico
+            chtGrafico.Series[0].Points.Clear();
+            
+            //Desenhar gráfico
+            for (int anos=1; anos<=34; anos ++)
+            {
+                SerieGrafico.Points.AddXY(eixoX[anos - 1], eixoY[anos - 1]);
+            }
+       
+        }
+
+       
+    }
+}
